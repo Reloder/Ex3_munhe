75fc3a1fbea6cf57c5ff86980830f143eae5a6c6
Maor Bakshi <maor_bakshi@walla.com>
2018-01-14 01:12:56 +0200

    upload the project

---
+++ src/Wigle.java
@@ -0,0 +1,30 @@
+
+// the information of each device;
+
+public class Wigle implements Comparable<Wigle> {
+	int RSSI = 0;
+ 	String MAC="", SSID="", AuthMode="", FirstSeen="", Channel="", Latitude="", Longitude="", Altitude="", Accuracy="", Type=""; 
+
+	 public Wigle(){
+		 
+	 }
+	 
+
+	 /*
+	  * implementaion of the comparbale interface by the strongest signal of the wigle record
+	  * @see java.lang.Comparable#compareTo(java.lang.Object)
+	  */
+	@Override
+	public int compareTo(Wigle obj1) {
+		if (this.RSSI > obj1.RSSI)
+			return 1;
+		if (this.RSSI < obj1.RSSI)
+			return -1;
+		return 0;
+	
+	}
+ }
+ 
+ 
+
+
